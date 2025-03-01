1584b34e0aab28357d30c5a333399d463e02bd19
Hanorah <ksdnorah@gmail.com>
2025-03-01 17:32:32 +0100

    commit

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,9 +2,8 @@
 /*                            External Dependencies                           */
 /* -------------------------------------------------------------------------- */
 
-import React, { useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import emailjs from 'emailjs-com'; // Import EmailJS
 
 /* -------------------------- Internal Dependencies ------------------------- */
 
@@ -12,31 +11,6 @@ import Layout, { PageWrapper } from '../components/Layout';
 import FooterLink from '../components/Footer';
 
 const Contact = () => {
-  const form = useRef<HTMLFormElement>(null); // Specify the type of the ref
-
-  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-    // Type the event parameter
-    e.preventDefault(); // Prevent default form submission
-
-    emailjs
-      .sendForm(
-        'service_hc42hyp', // Replace with your EmailJS Service ID
-        'template_1ao4v29', // Replace with your EmailJS Template ID
-        form.current!, // Use the non-null assertion operator since we know it's not null
-        'k-BWdsQZrHfEQhKHh' // Replace with your EmailJS User ID (Public Key)
-      )
-      .then(
-        (result) => {
-          console.log(result.text); // Handle success
-          alert('Email sent successfully!'); // You can customize this
-        },
-        (error) => {
-          console.log(error.text); // Handle error
-          alert('Failed to send email, please try again later.');
-        }
-      );
-  };
-
   return (
     <Layout title="Contact">
       <PageSection>
@@ -45,15 +19,14 @@ const Contact = () => {
           <article>
             <p>
               Get in touch or shoot me an email directly on{' '}
-              <b>
-                <a href="mailto:ksdnorah@gmail.com">ksdnorah@gmail.com</a>
-              </b>
+              <b>hellocodewonders@gmail.com</b>
             </p>
           </article>
           <br />
-          <form ref={form} onSubmit={sendEmail}>
-            {' '}
-            {/* Use the ref and onSubmit handler */}
+          <form
+            method="POST"
+            action="https://formspree.io/adenekanwonderful41@gmail.com"
+          >
             <div className="fields">
               <div className="field half">
                 <input
